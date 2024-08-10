f4bd59f47ff036b512d9e44fea65e794ed7a883b
Abdessamad Enabih <a.enabih@gmail.com>
2024-08-10 22:20:35 +0100

    init webscaping

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
-    fmt.Print("hello web crawler")
+	site_to_visit := []string{
+		"https://en.wikipedia.org/wiki/Albert_Stanley,_1st_Baron_Ashfield",
+		"https://en.wikipedia.org/wiki/Horsecar",
+	}
+
+	for _, url := range site_to_visit {
+		collector := colly.NewCollector()
+
+		// whenever the collector is about to make a new request
+		collector.OnRequest(func(r *colly.Request) {
+			// print the url of that request
+			fmt.Println("Visiting", r.URL)
+		})
+		collector.OnResponse(func(r *colly.Response) {
+			fmt.Println("Got a response from", r.Request.URL)
+		})
+		collector.OnError(func(r *colly.Response, e error) {
+			fmt.Println("Blimey, an error occurred!:", e)
+		})
+		collector.Visit(url)
+	}
+
 }
