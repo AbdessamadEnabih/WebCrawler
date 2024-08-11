ba5a0745fdd6c1a3cfe86994a80bd3b614e9d958
Abdessamad Enabih <a.enabih@gmail.com>
2024-08-11 20:51:05 +0100

    add WaitGroups and extractLinks()

--- main.go
+++ main.go
@@ -3,48 +3,47 @@ package main
 import (
 	"fmt"
 	"strings"
-
+	"sync"
 	"github.com/gocolly/colly"
 )
 
+func crawl(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	collector := colly.NewCollector()
+
+	extractLinks(collector)
+
+	collector.Visit(url)
+
+}
+
+func extractLinks(c *colly.Collector) []string {
+	var links []string
+	c.OnHTML("a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if strings.Contains(link, "https") {
+			fmt.Println(link)
+			links = append(links, link)
+		}
+	})
+	return links
+}
+
 func main() {
-	site_to_visit := []string{
+	var wg sync.WaitGroup
+
+	seedUrls := []string{
 		"https://en.wikipedia.org/wiki/Albert_Stanley,_1st_Baron_Ashfield",
 		// "https://en.wikipedia.org/wiki/Horsecar",
 	}
 
-	crawl(site_to_visit)
-
-	fmt.Print("Done Crawling !!!!!")
-}
-
-func crawl(site_to_visit []string) {
-	collector := colly.NewCollector()
-
-	collector.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	collector.OnResponse(func(r *colly.Response) {
-		fmt.Println("Got a response from", r.Request.URL)
-	})
-
-	collector.OnHTML("a", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		if strings.Contains(link, "https") {
-			fmt.Println(link)
-		}
-
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	collector.OnError(func(r *colly.Response, e error) {
-		fmt.Println("Blimey, an error occurred!:", e)
-	})
-
-	for index, url := range site_to_visit {
-		fmt.Printf("Website %d\n", index+1)
-		collector.Visit(url)
-		fmt.Print("----------------------------------")
+	for _, url := range seedUrls {
+		wg.Add(1)
+		go crawl(url, &wg)
 	}
+
+	wg.Wait()
+
+	fmt.Print("Done complete !!!!!")
 }
