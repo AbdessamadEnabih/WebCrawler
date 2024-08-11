bc12271018351770277ef3061b1a2bc132e0de2e
Abdessamad Enabih <a.enabih@gmail.com>
2024-08-11 19:56:51 +0100

    Crawl to sub links found in the initial urls

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -9,24 +10,39 @@ import (
 func main() {
 	site_to_visit := []string{
 		"https://en.wikipedia.org/wiki/Albert_Stanley,_1st_Baron_Ashfield",
-		"https://en.wikipedia.org/wiki/Horsecar",
+		// "https://en.wikipedia.org/wiki/Horsecar",
 	}
+
+	crawl(site_to_visit)
+
+	fmt.Print("Done Crawling !!!!!")
+}
+
+func crawl(site_to_visit []string) {
+	collector := colly.NewCollector()
+
+	collector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	
+	collector.OnResponse(func(r *colly.Response) {
+		fmt.Println("Got a response from", r.Request.URL)
+	})
+
+	collector.OnHTML("a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if strings.Contains(link, "https") {
+			fmt.Println(link)
+		}
+
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	collector.OnError(func(r *colly.Response, e error) {
+		fmt.Println("Blimey, an error occurred!:", e)
+	})
 
 	for _, url := range site_to_visit {
-		collector := colly.NewCollector()
-
-		// whenever the collector is about to make a new request
-		collector.OnRequest(func(r *colly.Request) {
-			// print the url of that request
-			fmt.Println("Visiting", r.URL)
-		})
-		collector.OnResponse(func(r *colly.Response) {
-			fmt.Println("Got a response from", r.Request.URL)
-		})
-		collector.OnError(func(r *colly.Response, e error) {
-			fmt.Println("Blimey, an error occurred!:", e)
-		})
 		collector.Visit(url)
 	}
-
 }
