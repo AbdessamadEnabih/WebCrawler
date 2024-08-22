74320bccbf64d77c4e1b9052de708909f2bc137c
Abdessamad Enabih <a.enabih@gmail.com>
2024-08-22 22:18:04 +0100

    Implemented detailed comments throughout the web crawler codebase to enhance clarity and maintainability. Added explanations for the use of sync.WaitGroup, mutex protection, and the crawling process logic, improving understanding of concurrency management and data protection mechanisms

--- main.go
+++ main.go
@@ -9,18 +9,27 @@ import (
 )
 
 var visited = make(map[string]bool)
+
+// Declare a mutex to protect the `visited` map from concurrent access, ensuring thread safety.
 var mu sync.Mutex
 
+// `crawl` function initiates the web crawling process for a given URL. It first checks if the URL has already been visited;
+// if not, it marks the URL as visited and then uses Colly to visit the URL and extract links. New URLs found are then
+// passed to the `crawl` function recursively to continue the crawling process.
 func crawl(url string, wg *sync.WaitGroup) {
+	// Defer the decrement of the WaitGroup counter to ensure it is called upon function completion,
+	// effectively signaling that one less goroutine is active.
 	defer wg.Done()
 	collector := colly.NewCollector()
 
+	// Lock the mutex to protect the `visited` map from concurrent access.
 	mu.Lock()
 	if visited[url] {
 		mu.Unlock()
 		return
 	}
 	visited[url] = true
+	// Unlock the mutex, allowing other goroutines to access the `visited` map.
 	mu.Unlock()
 
 	collector.Visit(url)
@@ -33,8 +42,12 @@ func crawl(url string, wg *sync.WaitGroup) {
 	}
 }
 
+// The function `extractLinks` uses Colly to extract and return all links with "https" from a webpage.
 func extractLinks(c *colly.Collector) []string {
 	var links []string
+
+	// Set up an HTML element handler to find all anchor tags ('<a>') and extract their href attributes.
+	// Only links starting with "https" are collected to filter out relative or invalid URLs.
 	c.OnHTML("a", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		if strings.Contains(link, "https") {
@@ -42,10 +55,13 @@ func extractLinks(c *colly.Collector) []string {
 			links = append(links, link)
 		}
 	})
+
 	return links
 }
 
 func main() {
+	// Initialize a WaitGroup to ensure the main thread waits for all goroutines to complete their execution,
+	// preventing the program from exiting prematurely and ensuring all web pages are fully crawled.
 	var wg sync.WaitGroup
 
 	seedUrls := []string{
@@ -58,6 +74,8 @@ func main() {
 		go crawl(url, &wg)
 	}
 
+	// Wait for all goroutines to complete their execution. This ensures that the program does not exit
+	// prematurely, allowing all web pages to be fully crawled and processed.
 	wg.Wait()
 
 	fmt.Print("Done complete !!!!!")
